76151fca45d5def15a5fea3faa7ec319afba8bba
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2020-03-28 19:14:43 +0100

    commit1

--- header.php
+++ header.php
@@ -1,37 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head();?>
-</head>
-<body>
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavId">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <div class="dropdown-menu" aria-labelledby="dropdownId">
-                    <a class="dropdown-item" href="#">Action 1</a>
-                    <a class="dropdown-item" href="#">Action 2</a>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <?php wp_head();?>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Lato:400,300,700,900" rel="stylesheet">
+    </head>
+
+    <body>
+        <header id="header" class="fixed-top">
+            <div class="container">
+
+                <div class="logo float-left">
+                    <h1 class="text-light"><a href="index.php"><span>Acceuil</span></a></h1>
+
                 </div>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-</nav>
+                <nav class="nav-menu float-right d-none d-lg-block">
+                    <ul>
+                        <li class="drop-down"><a href="#">Présentation</a>
+                            <ul>
+                                <li class="top_b"><a href=""> Présentation</a></li>
+                                <li><a href="">Mot du directeur</a></li>
+                                <li><a href="">ESTSB en chiffres </a></li>
+                            </ul>
+                        </li>
+                        <li class="drop-down"><a href="#">Formation</a>
+                                    <ul>
+                                        <li class="top_b"><a href=""> Formation intial</a></li>
+                                        <li><a href="">Formation secondaire</a></li>
+                                    </ul>
+                        </li>
+                        <li class="drop-down"><a href="#">Recherche</a>
+                            <ul>
+                                <li class="top_b"><a href="">Structures de recherche </a></li>
+                                <li><a href="">Centre des études Doctorales</a></li>
+                            </ul>
+                        </li>
+                        <li class="drop-down"><a href="#">Carrières</a>
+                            <ul>
+                                <li class="top_b"><a href=""> Stage</a></li>
+                                <li><a href="">Emploi</a></li>
+                            </ul>
+                        </li>
+                        <li ><a href="">Calendrier</a></li>
+                        <li ><a href="">Contact</a></li>
+                    </ul>
+
+                </nav>
+
+            </div>
+        </header> 
+        <br><br><b></b><br>
