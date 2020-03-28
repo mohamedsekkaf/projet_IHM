e86507783963ccae3019ff61dafc635c095b262a
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2020-03-28 21:21:04 +0100

    commit2

--- index.php
+++ index.php
@@ -1,50 +1,50 @@
  <?php get_header(); ?>
-     <div id="carouselId" class="carousel slide" data-ride="carousel">
-         <ol class="carousel-indicators">
-             <li data-target="#carouselId" data-slide-to="0" class="active"></li>
-             <li data-target="#carouselId" data-slide-to="1"></li>
-             <li data-target="#carouselId" data-slide-to="2"></li>
-         </ol>
-         <div class="carousel-inner" role="listbox">
-             <div class="carousel-item active">
-                 <img style="width:100%; height:70vh" src="<?php echo get_template_directory_uri()?>/image/img1.jpg"
-                     alt="First slide">
-                 <div class="carousel-caption d-none d-md-block">
-                     <h3 style>DUT Génie Informatique </h3>
-                     <p>Description</p>
-                 </div>
-             </div>
-             <div class="carousel-item">
-                 <img style="width:100%; height:70vh" src="<?php echo get_template_directory_uri()?>/image/img2.jpeg"
-                     alt="Second slide">
-                 <div class="carousel-caption d-none d-md-block">
-                     <h3>DUT technique de Management</h3>
-                     <p>Description</p>
-                 </div>
-             </div>
-             <div class="carousel-item">
-                 <img style="width:100%; height:70vh" src="<?php echo get_template_directory_uri()?>/image/img3.jpg"
-                     alt="Third slide">
-                 <div class="carousel-caption d-none d-md-block">
-                     <h3>DUT GA</h3>
-                     <p>Description</p>
-                 </div>
+ <div id="carouselId" class="carousel slide" data-ride="carousel">
+     <ol class="carousel-indicators">
+         <li data-target="#carouselId" data-slide-to="0" class="active"></li>
+         <li data-target="#carouselId" data-slide-to="1"></li>
+         <li data-target="#carouselId" data-slide-to="2"></li>
+     </ol>
+     <div class="carousel-inner" role="listbox">
+         <div class="carousel-item active">
+             <img style="width:100%; height:70vh" src="<?php echo get_template_directory_uri()?>/image/img1.jpg"
+                 alt="First slide">
+             <div class="carousel-caption d-none d-md-block">
+                 <h3 style>DUT Génie Informatique </h3>
+                 <p>Description</p>
              </div>
          </div>
-         <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
-             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-             <span class="sr-only">Previous</span>
-         </a>
-         <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
-             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-             <span class="sr-only">Next</span>
-         </a>
+         <div class="carousel-item">
+             <img style="width:100%; height:70vh" src="<?php echo get_template_directory_uri()?>/image/img2.jpeg"
+                 alt="Second slide">
+             <div class="carousel-caption d-none d-md-block">
+                 <h3>DUT technique de Management</h3>
+                 <p>Description</p>
+             </div>
+         </div>
+         <div class="carousel-item">
+             <img style="width:100%; height:70vh" src="<?php echo get_template_directory_uri()?>/image/img3.jpg"
+                 alt="Third slide">
+             <div class="carousel-caption d-none d-md-block">
+                 <h3>DUT GA</h3>
+                 <p>Description</p>
+             </div>
+         </div>
      </div>
-     <br><br><br>
-     <div class="container">
-         <h1 style="text-align:center;">ACTUALITÉS</h1>
-     </div>
-     <hr style=" width: 20vh;
+     <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="sr-only">Previous</span>
+     </a>
+     <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="sr-only">Next</span>
+     </a>
+ </div>
+ <br><br><br>
+ <div class="container">
+     <h1 style="text-align:center;">ACTUALITÉS</h1>
+ </div>
+ <hr style=" width: 20vh;
   height: 10px;
   color: red;
   display: block;
@@ -55,54 +55,91 @@
   border-style: inset;
   border-width: 2px;
   background:rgb(38, 207, 236);">
-
-
-     <!--      09540348yt938qhfuoew-->
-     <div class="container">
-         <div class="row">
-             <div class="col-lg-3 col-sm-6">
-                 <div style="text-align: center;box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.25);" class="card">
-                     <img class="card-img-top" src="holder.js/100x180/" alt="">
-                     <div class="card-body">
-                         <h4 class="card-title">Avis Aux Etudiant</h4>
-                         <p class="card-text">Text</p>
-                     </div>
-                 </div>
-             </div>155052524831
-             <br><br>
-             <div class="col-lg-3 col-sm-6 ">
-                 <div style="text-align: center;box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.25);" class="card">
-                     <img class="card-img-top" src="holder.js/100x180/" alt="">
-                     <div class="card-body">
-                         <h4 class="card-title">Avis Aux Etudiant</h4>
-                         <p class="card-text">Text</p>
-                     </div>
+ <!--09540348yt938qhfuoew-->
+ <div class="container">
+     <div class="row">
+         <div class="col-lg-3 col-sm-6">
+             <div style="text-align: center;box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.25);" class="card">
+                 <img class="card-img-top" src="holder.js/100x180/" alt="">
+                 <div class="card-body">
+                     <h4 class="card-title">Avis Aux Etudiant</h4>
+                     <p class="card-text">Text</p>
                  </div>
              </div>
-             <br><br>
-             <div class="col-lg-3 col-sm-6 ">
-                 <div style="text-align: center;box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.25);" class="card">
-                     <img class="card-img-top" src="holder.js/100x180/" alt="">
-                     <div class="card-body">
-                         <h4 class="card-title">Avis Aux Etudiant</h4>
-                         <p class="card-text">Text</p>
-                     </div>
+         </div>15155052524831
+         <br><br>
+         <div class="col-lg-3 col-sm-6 ">
+             <div style="text-align: center;box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.25);" class="card">
+                 <img class="card-img-top" src="holder.js/100x180/" alt="">
+                 <div class="card-body">
+                     <h4 class="card-title">Avis Aux Etudiant</h4>
+                     <p class="card-text">Text</p>
                  </div>
              </div>
-             <br><br>
-             <div class="col-lg-3 col-sm-6">
-                 <div style="text-align: center;box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.25);" class="card">
-                     <img class="card-img-top" src="holder.js/100x180/" alt="">
-                     <div class="card-body">
-                         <h4 class="card-title">Avis Aux Etudiant</h4>
-                         <p class="card-text">Text</p>
-                     </div>
+         </div>
+         <br><br>
+         <div class="col-lg-3 col-sm-6 ">
+             <div style="text-align: center;box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.25);" class="card">
+                 <img class="card-img-top" src="holder.js/100x180/" alt="">
+                 <div class="card-body">
+                     <h4 class="card-title">Avis Aux Etudiant</h4>
+                     <p class="card-text">Text</p>
+                 </div>
+             </div>
+         </div>
+         <br><br>
+         <div class="col-lg-3 col-sm-6">
+             <div style="text-align: center;box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.25);" class="card">
+                 <img class="card-img-top" src="holder.js/100x180/" alt="">
+                 <div class="card-body">
+                     <h4 class="card-title">Avis Aux Etudiant</h4>
+                     <p class="card-text">Text</p>
                  </div>
              </div>
          </div>
      </div>
-     <!--   jerhfoguhewriqubfjkqwerjk-->
-<div class="test"></div>
+ </div>
+ <!--   jerhfoguhewriqubfjkqwerjk-->
+ <div class="test"></div>
+ <div class="container">
+ <table>
+         <tr>
+             <td><img style="width:100px; height:100px;display:inline-block;"
+                     src="<?php echo get_template_directory_uri()?>/image/img2.jpeg" alt="Second slide"></td>
+             <td>
+                 <h1>title title</h1>
+                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae debitis eos nobis possimus officiis
+                 quos quo adipisci porro iusto voluptatum? Odio ducimus nulla quidem perferendis porro doloribus,
+                 provident voluptate voluptates.
+             </td>
+         </tr>
+         </table>
+         <br><br>
+         <table>
+         <tr>
+             <td><img style="width:100px; height:100px;display:inline-block;"
+                     src="<?php echo get_template_directory_uri()?>/image/img2.jpeg" alt="Second slide"></td>
+             <td>
+                 <h1>title title</h1>
+                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae debitis eos nobis possimus officiis
+                 quos quo adipisci porro iusto voluptatum? Odio ducimus nulla quidem perferendis porro doloribus,
+                 provident voluptate voluptates.
+             </td>
+         </tr>
+         </table>
+         <br><br>
+         <table>
+         <tr>
+             <td><img style="width:100px; height:100px;display:inline-block;"
+                     src="<?php echo get_template_directory_uri()?>/image/img2.jpeg" alt="Second slide"></td>
+             <td>
+                 <h1>title title</h1>
+                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae debitis eos nobis possimus officiis
+                 quos quo adipisci porro iusto voluptatum? Odio ducimus nulla quidem perferendis porro doloribus,
+                 provident voluptate voluptates.
+             </td>
+         </tr>
+ </table>
 
-     <?php get_footer(); ?>
+ <?php get_footer(); ?>
  </body>
