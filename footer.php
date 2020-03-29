121c6e77912edea9477009c343e54c451baefe54
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2020-03-29 23:51:11 +0100

    cmmit7

--- footer.php
+++ footer.php
@@ -4,29 +4,24 @@
          
          <div class="container">
          <div class="row">
-            	
-              <div class="col-md-5">
-                        <p class="small-text pull-left"><font color='#8ab5d6'>&copy;  Ecole Supérieure de Technologie Sidi Bennour</font> </p>
-                        
+              <div class="col-12">
+                    <p style="text-align:center;" class="small-text pull-lef "><font color='#8ab5d6'>&copy;  Ecole Supérieure de Technologie Sidi Bennour</font> </p>
                     </div> <!-- /.col-md-5 -->
-                    <div class="col-md-7">
-              
-                        <ul class="footer-nav">
-                            <li><a href="actualite.php"><b>Actualité</b></a></li>
-                              <li><a href="contact.php"><b>Contact</b></a></li>
+                    <div class="col-12">
+                        <ul style="text-align:center; text-decoration: none;list-style-type:none" class="footer-nav drop-down">
+                            <li style="text-decoration: none;list-style-type:none" ><a href="actualite.php"><b>Actualité</b></a></li>
+                              <li style="text-decoration: none;list-style-type:none"><a href="contact.php"><b>Contact</b></a></li>
                         </ul>
                                  
             </div> <!-- /.row -->
+            </div>
      <div class="col-12 center">
-     <span></span>
      </div>
      <!-- mawa9i3 tawasol ijtima3i!!!!!!!! -->
             <div class="head d-flex justify-content-center">
                 <div class="row" style="display: block!important;">
                     <a href="https://facebook.com"><img class="circle" src="<?php echo get_template_directory_uri()?>/image/facebook.png" width="40" height="40"  /></a>
-                    <a href=""> <img class="circle" src=""  /></a>
-                    <a href=""> <img class="circle" src="" /></a>
-                    <a href="https://www.instagram.com"><img class="circle" src="<?php echo get_template_directory_uri()?>/image/instagram.png" width="40" height="40"  /></a>
+                    <a href="https://www.instagram.com"><img class="circle" src="<?php echo get_template_directory_uri()?>/image/instagram.svg" width="40" height="40"/></a>
                     <a href="estsb.ucd@gmail.com"> <img class="circle" src=""  /></a>
                     <a href=""> <img class="circle" src="" /></a>
                 </div>
